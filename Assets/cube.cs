cb12e610062c841ad5aebbdfa02ca46383d4511b
liamhanninen <liam.hanninen@gmail.com>
2017-02-18 11:43:14 -0600

    fences, roads, towers

---
+++ Assets/cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //print(Input.GetAxis("HorizontalUI"));
+        
+        transform.Translate(0f, Input.GetAxis("VerticalUI") * Time.deltaTime, 0f);
+
+    }
+}
