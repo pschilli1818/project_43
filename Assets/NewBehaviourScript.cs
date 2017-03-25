457dba1068b20750af7d317e5e56ee04dd9b35a4
patrick schilling <patrick schilling>
2017-03-25 11:50:17 -0500

    downtown set

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//print ("yo im a cube for real");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		transform.Translate(0f,Input.GetAxis("VerticalUI")* Time.deltaTime,0f);
+	}
+}
