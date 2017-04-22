2622d03e164f09972e3d290978ff4deafd11cefa
liamhanninen <liam.hanninen@gmail.com>
2017-04-22 12:36:03 -0500

    building skins

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(Input.GetAxis("HorizontalUI"));
+
+      transform.Translate(Input.GetAxis("HorizontalUI") * Time.deltaTime, 0f, 0f);
+    }
+}
